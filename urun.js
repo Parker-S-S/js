f5f1bc7ffd4e5adcbd16f2aac7c84359e6235929
Parker-S-S <152989713+Parker-S-S@users.noreply.github.com>
2024-11-11 11:38:38 -0600

    Update urun.js

--- urun.js
+++ urun.js
@@ -45,4 +45,4 @@ document.addEventListener("keydown", function (r) {
 }});
 
 // execute on load
-(function () { var script = document.createElement('script'); script.src='//cdn.jsdelivr.net/npm/eruda'; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
+document.onload((function () { var script = document.createElement('script'); script.src='//cdn.jsdelivr.net/npm/eruda'; document.body.appendChild(script); script.onload = function () { eruda.init() } })());
